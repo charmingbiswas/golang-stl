14800cb95c02d9b464ff63d547e5847dd6ee1548
Riyansh Biswas <charmingbiswas@gmail.com>
2025-07-25 19:09:38 +0530

    Re-implemented generic queue and stack

--- queue/queue.go
+++ queue/queue.go
@@ -1,1 +1,35 @@
 package queue
+
+type queue[T any] struct {
+	data []T
+}
+
+func NewQueue[T any]() *queue[T] {
+	return &queue[T]{
+		data: make([]T, 0, 10),
+	}
+}
+
+func (this *queue[T]) Push(val T) {
+	this.data = append(this.data, val)
+}
+
+func (this *queue[T]) Pop() bool {
+	if len(this.data) == 0 {
+		return false
+	}
+	this.data = this.data[1:]
+	return true
+}
+
+func (this *queue[T]) Front() (T, bool) {
+	if len(this.data) == 0 {
+		return *new(T), false
+	}
+
+	return this.data[0], true
+}
+
+func (this *queue[T]) IsEmpty() bool {
+	return len(this.data) == 0
+}
--- stack/stack.go
+++ stack/stack.go
@@ -1,1 +1,35 @@
 package stack
+
+type stack[T any] struct {
+	data []T
+}
+
+func NewQueue[T any]() *stack[T] {
+	return &stack[T]{
+		data: make([]T, 0, 10),
+	}
+}
+
+func (this *stack[T]) Push(val T) {
+	this.data = append(this.data, val)
+}
+
+func (this *stack[T]) Pop() bool {
+	if len(this.data) == 0 {
+		return false
+	}
+	this.data = this.data[:len(this.data)]
+	return true
+}
+
+func (this *stack[T]) Top() (T, bool) {
+	if len(this.data) == 0 {
+		return *new(T), false
+	}
+
+	return this.data[len(this.data)-1], true
+}
+
+func (this *stack[T]) IsEmpty() bool {
+	return len(this.data) == 0
+}
