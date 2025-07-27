7184b38737a9dcf1d7ef49cfa49a61e4cde88f6a
Riyansh Biswas <charmingbiswas@gmail.com>
2025-07-27 20:46:48 +0530

    emptied main.go file

--- cmd/main.go
+++ cmd/main.go
@@ -1,13 +1,6 @@
 // This package main will only be used for manual testing of implemented Golang data structures and algorithms
 package main
 
-import (
-	"fmt"
-	"reflect"
-)
-
 // Do manual testing of your data structures and algorithms here
 func main() {
-	v := []int{7}
-	fmt.Println(reflect.DeepEqual(v, []int{7}))
 }
