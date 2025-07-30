a60311ae9ef111cc32d9174bccae5d35aa031dd6
Riyansh Biswas <charmingbiswas@gmail.com>
2025-07-31 01:24:40 +0530

    implemented generic versions of binary search, lower bound and upper bound algorithms

--- algo/binary_search.go
+++ algo/binary_search.go
@@ -2,6 +2,8 @@ package algo
 
 import "cmp"
 
+// Standard binary search algorithm.
+// Works with in-built comparable types.
 func BinarySearch[T cmp.Ordered](array []T, target T) (int, bool) {
 	low := 0
 	high := len(array) - 1
@@ -20,6 +22,8 @@ func BinarySearch[T cmp.Ordered](array []T, target T) (int, bool) {
 	return 0, false
 }
 
+// Standard binary search lowerbound algorithm.
+// Works with in-built comparable types.
 func LowerBound[T cmp.Ordered](array []T, target T) int {
 	low := 0
 	high := len(array) - 1
@@ -37,6 +41,8 @@ func LowerBound[T cmp.Ordered](array []T, target T) int {
 	return low
 }
 
+// Standard binary search upperbound algorithm.
+// Works with in-built comparable types.
 func UpperBound[T cmp.Ordered](array []T, target T) int {
 	low := 0
 	high := len(array)
@@ -45,6 +51,48 @@ func UpperBound[T cmp.Ordered](array []T, target T) int {
 		mid := low + (high-low)/2
 
 		if array[mid] <= target {
+			low = mid + 1
+		} else {
+			high = mid
+		}
+	}
+
+	return low
+}
+
+// Standard binary search lowerbound algorithm.
+// Takes a 'comparator' function which decides how two custom values are compared.
+// If the two values are equal, comparator function should return 0.
+// If A is less than B, comparator should return -1.
+// If A is greater than B, comparator should return 1.
+func LowerBoundWithFunc[T any](array []T, target T, comparator func(A, B T) int) int {
+	low := 0
+	high := len(array) - 1
+
+	for low < high {
+		mid := low + (high-low)/2
+		if comparator(array[mid], target) == -1 {
+			low = mid + 1
+		} else {
+			high = mid
+		}
+	}
+
+	return low
+}
+
+// Standard binary search upperbound algorithm.
+// Takes a 'comparator' function which decides how two custom values are compared.
+// If the two values are equal, comparator function should return 0.
+// If A is less than B, comparator should return -1.
+// If A is greater than B, comparator should return 1.
+func UpperBoundWithFunc[T any](array []T, target T, comparator func(A, B T) int) int {
+	low, high := 0, len(array)-1
+
+	for low < high {
+		mid := low + (high-low)/2
+
+		if comparator(array[mid], target) == 0 || comparator(array[mid], target) == -1 {
 			low = mid + 1
 		} else {
 			high = mid
