2aef39a43151930cd765627ede924ddb30eb21d1
Riyansh Biswas <charmingbiswas@gmail.com>
2025-07-31 23:57:10 +0530

    fixed binary search algorithm logic and it's related variants

--- algo/binary_search.go
+++ algo/binary_search.go
@@ -4,22 +4,22 @@ import "cmp"
 
 // Standard binary search algorithm.
 // Works with in-built comparable types.
-func BinarySearch[T cmp.Ordered](array []T, target T) (int, bool) {
+func BinarySearch[T cmp.Ordered](array []T, target T) int {
 	low := 0
 	high := len(array) - 1
 
-	for low < high {
+	for low <= high {
 		mid := low + (high-low)/2
 
 		if target == array[mid] {
-			return mid, true
+			return mid
 		} else if target < array[mid] {
 			high = mid - 1
 		} else {
 			low = mid + 1
 		}
 	}
-	return 0, false
+	return -1
 }
 
 // Standard binary search lowerbound algorithm.
@@ -28,14 +28,20 @@ func LowerBound[T cmp.Ordered](array []T, target T) int {
 	low := 0
 	high := len(array) - 1
 
-	for low < high {
+	for low <= high {
 		mid := low + (high-low)/2
 
 		if array[mid] < target {
 			low = mid + 1
-		} else {
-			high = mid
+		} else if array[mid] > target {
+			high = mid - 1
+		} else if array[mid] == target {
+			high = mid - 1
 		}
+	}
+
+	if low > len(array) || array[low] != target {
+		return -1
 	}
 
 	return low
@@ -45,59 +51,23 @@ func LowerBound[T cmp.Ordered](array []T, target T) int {
 // Works with in-built comparable types.
 func UpperBound[T cmp.Ordered](array []T, target T) int {
 	low := 0
-	high := len(array)
-
-	for low < high {
-		mid := low + (high-low)/2
-
-		if array[mid] <= target {
-			low = mid + 1
-		} else {
-			high = mid
-		}
-	}
-
-	return low
-}
-
-// Standard binary search lowerbound algorithm.
-// Takes a 'comparator' function which decides how two custom values are compared.
-// If the two values are equal, comparator function should return 0.
-// If A is less than B, comparator should return -1.
-// If A is greater than B, comparator should return 1.
-func LowerBoundWithFunc[T any](array []T, target T, comparator func(A, B T) int) int {
-	low := 0
 	high := len(array) - 1
 
-	for low < high {
+	for low <= high {
 		mid := low + (high-low)/2
-		if comparator(array[mid], target) == -1 {
+
+		if array[mid] < target {
 			low = mid + 1
-		} else {
-			high = mid
+		} else if array[mid] > target {
+			high = mid - 1
+		} else if array[mid] == target {
+			low = mid + 1
 		}
 	}
 
-	return low
-}
-
-// Standard binary search upperbound algorithm.
-// Takes a 'comparator' function which decides how two custom values are compared.
-// If the two values are equal, comparator function should return 0.
-// If A is less than B, comparator should return -1.
-// If A is greater than B, comparator should return 1.
-func UpperBoundWithFunc[T any](array []T, target T, comparator func(A, B T) int) int {
-	low, high := 0, len(array)-1
-
-	for low < high {
-		mid := low + (high-low)/2
-
-		if comparator(array[mid], target) == 0 || comparator(array[mid], target) == -1 {
-			low = mid + 1
-		} else {
-			high = mid
-		}
+	if high < 0 || array[high] != target {
+		return -1
 	}
 
-	return low
+	return high
 }
